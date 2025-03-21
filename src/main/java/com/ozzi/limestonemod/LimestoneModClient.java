9b02ffaeb2c51de7c53110598b402c0a1de7072f
0zMC <ozzimcyt@gmail.com>
2025-03-21 09:46:34 -0600

    Initial commit

---
+++ src/main/java/com/ozzi/limestonemod/LimestoneModClient.java
@@ -0,0 +1,10 @@
+package com.ozzi.limestonemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LimestoneModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/ozzi/limestonemod/LimestoneModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ozzi.limestonemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LimestoneModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
